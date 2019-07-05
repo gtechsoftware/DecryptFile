7d8a5dea54c538d8c890d8b7ab45b8fdf72f7d7e
Michael Powell <mpowell-programming@comcast.net>
2019-07-05 14:00:24 -0700

    Commit initial version of DecryptFile

--- DecryptFile/ViewController.h
+++ DecryptFile/ViewController.h
@@ -7,9 +7,16 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "RNCryptor.h"
+#import "RNDecryptor.h"
 
 @interface ViewController : NSViewController
-
-
+@property   IBOutlet    NSTextField *fileTextField;
+@property   IBOutlet    NSTextField *passwordTextField;
+@property   IBOutlet   NSButton    *selectButton;
+@property   IBOutlet    NSButton    *decryptButton;
+@property   IBOutlet    NSTextField    *outputText;
+-(IBAction)selectButtonPressed:(id)sender;
+-(IBAction)decryptButtonPressed:(id)sender;
 @end
 
--- DecryptFile/ViewController.m
+++ DecryptFile/ViewController.m
@@ -9,11 +9,31 @@
 #import "ViewController.h"
 
 @implementation ViewController
-
+@synthesize fileTextField;
+@synthesize passwordTextField;
+@synthesize selectButton;
+@synthesize decryptButton;
+@synthesize outputText;
+//
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    // Do any additional setup after loading the view.
+    NSRect ourRect;
+    ourRect = self.selectButton.frame;
+    ourRect.size.width = 200;
+//    self.selectButton.frame = ourRect;
+    self.selectButton.title = @"Select File";
+    ourRect = self.decryptButton.frame;
+    ourRect.size.width = 200;
+//    self.decryptButton.frame = ourRect;
+    self.decryptButton.title = @"Decrypt File";
+    ourRect = self.passwordTextField.frame;
+    ourRect.size.width = 200;
+//    self.passwordTextField.frame = ourRect;
+    ourRect.origin.x = 20;
+    ourRect.origin.y = 500;
+    ourRect.size.height = 500;
+    ourRect.size.width = 500;
+//    self.outputText.frame = ourRect;
 }
 
 
@@ -23,5 +43,84 @@
     // Update the view, if already loaded.
 }
 
+-(IBAction)selectButtonPressed:(id)sender {
 
+    NSLog(@"selectButtonPressed");
+    NSOpenPanel *ourPanel = [NSOpenPanel openPanel];
+    ourPanel.title = @"Choose a file";
+    ourPanel.showsResizeIndicator = YES;
+    ourPanel.allowedFileTypes = @[@"xml"];
+    ourPanel.canChooseFiles = YES;
+    
+    NSURL   *ourURL;
+    NSString  *ourPath;
+    
+    if ([ourPanel runModal] == NSModalResponseOK) {
+        ourURL = ourPanel.URL;
+        if (ourURL != nil) {
+            ourPath = ourURL.path;
+            [self.fileTextField setStringValue:ourPath];
+        }   //  non nil URL
+    }   //  responseOK
+}   //  selectButtonPressed
+
+-(IBAction)  decryptButtonPressed:(id)sender {
+    
+    NSLog(@"decryptButtonPressed");
+    NSString *ourFilePath = [self.fileTextField stringValue];
+    NSString *ourPassWord = [self.passwordTextField stringValue];
+    NSError *ourError = nil;
+    NSString *outputString = nil;
+    if (ourFilePath) {
+        NSURL *ourFileURL = [NSURL fileURLWithPath:ourFilePath];
+        if (ourFileURL) {
+            NSLog(@"have ourFileURL");
+            NSString *ourFileContents = [NSString stringWithContentsOfURL:ourFileURL encoding:NSUTF8StringEncoding error:nil];
+            if (ourFileContents) {
+                NSLog(@"got ourFileContents");
+                //NSLog(ourFileContents);
+                NSData *encryptedData = [[NSData alloc] initWithBase64EncodedString:ourFileContents options:0];
+                if (encryptedData) {
+                    NSLog(@"got encyptedData");
+                    //NSLog(encryptedData.description);
+                    NSData *decryptedData = [RNDecryptor decryptData:encryptedData withPassword:ourPassWord error:&ourError];
+                    if (decryptedData) {
+                        NSLog(@"got decryptedData");
+                        //NSLog(decryptedData.description);
+                        NSString  *decryptedString = [[NSString alloc] initWithData:decryptedData encoding:NSUTF8StringEncoding];
+                        if (decryptedString && [decryptedString length] > 0) {
+                            NSLog(@"have decryptedString");
+                            outputString = decryptedString;
+                        }   //  have decryptedString
+                        else    {
+                            NSLog(@"decryptedString is nil or empty");
+                            outputString = @"decryptedString is nil or empty";
+                        }   //  have decryptedString
+                    }   //  got decryptedData
+                    else    {
+                        NSLog(@"could not get decryptedData");
+                        outputString = @"could not get decryptedData";
+                    }   //  could not get decryptedData
+                }   //  got encryptedData
+                else    {
+                    NSLog(@"could not get encryptedData");
+                    outputString = @"could not get encryptedData";
+                }   //  could not get encryptedData
+            }   //  got ourFileContents
+            else    {
+                NSLog(@"could not get ourFileContents");
+                outputString = @"could not get ourFileContents";
+            }   //  couldn't get ourFileContents
+        }   //  we have a url
+        else    {
+            NSLog(@"no URL");
+            outputString = @"no URL";
+        }   //  no URL
+    }   //  we have a filePath
+    else    {
+        NSLog(@"no filePath");
+        outputString = @"no filePath";
+    }   //  we have no filePath
+    [self.outputText setStringValue:outputString];
+}   //  decryptButtonPressed
 @end
